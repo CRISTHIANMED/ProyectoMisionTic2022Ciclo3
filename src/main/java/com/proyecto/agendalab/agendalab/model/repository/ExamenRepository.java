b3f71685ba3ff29425ced8cd589c5645a47e09b4
Cristhian Medina <cristhian.medina.or@hotmail.com>
2022-10-03 03:50:30 -0500

    nuevo

---
+++ src/main/java/com/proyecto/agendalab/agendalab/model/repository/ExamenRepository.java
@@ -0,0 +1,13 @@
+package com.proyecto.agendalab.agendalab.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.proyecto.agendalab.agendalab.model.entity.Examen;
+
+public interface ExamenRepository extends JpaRepository<Examen, Long> {
+        
+    //List<Examen> findAllBySeccion(Long seccionId);
+
+    //List<Examen> findAllByCategoryId(List<Long> seccionId);
+   
+}
---
+++ src/main/java/com/proyecto/agendalab/agendalab/service/ExamenService.java
@@ -0,0 +1,20 @@
+package com.proyecto.agendalab.agendalab.service;
+
+import java.util.List;
+
+import com.proyecto.agendalab.agendalab.controller.dto.ExamenDto;
+
+public interface ExamenService {
+
+    void guardarExamen(ExamenDto examen);
+    void editarExamen (ExamenDto examen);
+
+    void borrarExamen(Long cups);
+
+    List<ExamenDto> listarExamenes();
+
+    // List<ExamenDto> getExamenBySeccionId(Integer seccionId);
+
+    // ExamenDto getExamenById(String id);
+    
+}
